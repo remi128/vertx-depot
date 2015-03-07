e9cdbc13fcf806fed203b2d51fff096931353c28
Geoffrey Clements <baldmountain@gmail.com>
2015-03-07 15:42:05 -0500

    Kill off sqlite stuff, use mongo instead

--- src/main/java/com/baldmountain/depot/DepotVerticle.java
+++ src/main/java/com/baldmountain/depot/DepotVerticle.java
@@ -1,7 +1,5 @@
 package com.baldmountain.depot;
 
-import java.math.BigDecimal;
-
 import io.vertx.codetrans.annotations.CodeTranslate;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.json.JsonObject;
@@ -14,9 +12,6 @@ import io.vertx.ext.apex.sstore.LocalSessionStore;
 import io.vertx.ext.auth.AuthService;
 import io.vertx.ext.auth.shiro.ShiroAuthRealmType;
 import io.vertx.ext.auth.shiro.ShiroAuthService;
-import io.vertx.ext.jdbc.JdbcService;
-import io.vertx.ext.sql.ResultSet;
-import io.vertx.ext.sql.SqlConnection;
 
 /**
  * @author Geoffrey Clements
@@ -27,23 +22,13 @@ public class DepotVerticle extends AbstractVerticle {
     public static void main(String[] args) {
         Runner.runExample(DepotVerticle.class);
     }
-    
-    private JdbcService service;
+
     private static final Logger log = LoggerFactory.getLogger(DepotVerticle.class);
-    
+
     @CodeTranslate
     @Override
     public void start() throws Exception {
 
-        JsonObject config = new JsonObject()
-                .put("url", "jdbc:sqlite:test.db")
-                .put("driver_class", "org.sqlite.JDBC")
-                .put("max_pool_size", 30);
-
-        service = JdbcService.create(vertx, config);
-        service.start();
-        checkDatabase();
-        
         final Router router = Router.router(vertx);
 
         // We need cookies, sessions and request bodies
@@ -73,71 +58,14 @@ public class DepotVerticle extends AbstractVerticle {
         router.route("/logout").handler(context -> {
             context.session().logout();
             // Redirect back to the index page
-                context.response().putHeader("location", "/")
-                        .setStatusCode(302).end();
-            });
+            context.response().putHeader("location", "/")
+                    .setStatusCode(302).end();
+        });
 
         // Serve the non private static pages
         router.route().handler(StaticHandler.create());
 
         vertx.createHttpServer().requestHandler(router::accept).listen(8080);
     }
-    
-    private void checkDatabase() {
-        service.getConnection(res -> {
-            if (res.succeeded()) {
 
-              SqlConnection connection = res.result();
-              connection.execute("CREATE TABLE IF NOT EXISTS products(id INTEGER PRIMARY KEY ASC, title STRING, description TEXT, imageUrl STRING, price DECIMAL);", result -> {
-                 if (result.succeeded()) {
-                     connection.query("SELECT * FROM products", res2 -> {
-                         if (res2.succeeded()) {
-                             ResultSet rs = res2.result();
-                             log.info("We got " + rs.getNumRows() + " rows");
-                             if (rs.getNumRows() == 0) {
-                                 connection.execute("INSERT INTO products (title,description,price) VALUES ('Herland', 'A feminist novel', 2.00 );" , res3 -> {
-                                     if (res3.succeeded()) {
-                                         log.info(">> inserted");
-                                     } else {
-                                         log.error(">> didn't insert "+res3.cause().getMessage());
-                                     }
-                                 });
-                             } else {
-                                 rs.getResults().stream().forEach(row -> {
-                                     log.info(">> id: "+row.getInteger(0));
-                                     log.info(">> title: "+row.getString(1));
-                                     log.info(">> description: "+row.getString(2));
-                                     log.info(">> price: "+new BigDecimal(row.getDouble(4)));
-                                 });
-                             }
-                         }
-                     });
-                     
-                 } else {
-                     log.error("Error creating missing table"+result.cause().getMessage());
-                     connection.close(null);
-                     service.stop();
-                     try {
-                        stop();
-                    } catch (Exception e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
-                 }
-              });
-              // Got a connection
-
-            } else {
-              // Failed to get connection - deal with it
-                log.error("Can't get a connection to the DB");
-                service.stop();
-                try {
-                    stop();
-                } catch (Exception e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            }
-          });
-        }
 }
