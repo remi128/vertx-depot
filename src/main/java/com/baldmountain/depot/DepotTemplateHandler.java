cac86454d4e8f115c4d7c6fa7c8655ef81412ddc
Geoffrey Clements <baldmountain@gmail.com>
2015-03-14 08:25:53 -0400

    Add MIT License

--- src/main/java/com/baldmountain/depot/DepotTemplateHandler.java
+++ src/main/java/com/baldmountain/depot/DepotTemplateHandler.java
@@ -7,7 +7,29 @@ import io.vertx.ext.apex.impl.Utils;
 import io.vertx.ext.apex.templ.TemplateEngine;
 
 /**
- * Created by gclements on 3/10/15.
+ * @author Geoffrey Clements
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Geoffrey Clements
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  *
  */
 public class DepotTemplateHandler implements TemplateHandler {
